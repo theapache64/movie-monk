dca5a30244d0c2a438cf329da4686738f09c17dc
theapache64 <theapache64@gmail.com>
2020-04-28 11:10:56 +0530

    🌄 turn on pretty printing

--- src/main/kotlin/com/theapache64/moviemonk/utils/GsonUtil.kt
+++ src/main/kotlin/com/theapache64/moviemonk/utils/GsonUtil.kt
@@ -3,5 +3,7 @@ package com.theapache64.moviemonk.utils
 import com.google.gson.GsonBuilder
 
 object GsonUtil {
-    val gson = GsonBuilder().create()
+    val gson = GsonBuilder()
+        .setPrettyPrinting()
+        .create()
 }
